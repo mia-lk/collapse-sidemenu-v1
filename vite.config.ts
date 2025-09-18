e3deb5cc5e18f616883bd954e6ccfbf0182da929
Mia Lachkramaric <mia.lachkramaric@jll.com>
2025-09-18 13:17:14 +0200

    Fix GitHub Pages base path to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES === 'true' ? '/collapse-sidemenu/' : '/',
+  base: process.env.GITHUB_PAGES === 'true' ? '/collapse-sidemenu-v1/' : '/',
 })
 
